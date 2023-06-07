2775b5653a188956318a34ea6e7400fed4abf222
PPatel4 <ppatel4@scgcontractor.com>
2023-06-07 14:41:58 -0700

    first commit

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var app = express()
 
 
 app.get("/", function(req, res) {
-    res.send('hello word latest v1 force automated')
+    res.send('hello word latest v1 force automated1')
 })
 
 app.listen(80, function(req, res) {
